6486dca19e6f5b83b103ae79a53b6dca7c04dc91
wdxGit <1091830282@qq.com>
2019-05-23 16:45:37 +0800

    更新到jsx代码优化

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
 class ToDoItem extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this); //指向修改
+  }
+
   render() {
+    const {content} = this.props;
     return (
-      <div>
-        item
+      <div onClick={this.handleClick}>
+        {content}
       </div>
     );
+  }
+
+  handleClick() {
+    const { deleteItem, index } = this.props; //简化写法
+    deleteItem(index)  //子组件像父组件 接收方法
   }
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,6 @@
 import React,{ Component, Fragment } from 'react';
 import './style.css';
+import ToDoItem from './ToDoItem'
 
 class TodoList extends Component {
 	// 定义数据
@@ -9,6 +10,9 @@ class TodoList extends Component {
 			inputValue: '123', //input数据
 			list:['学习react', 'helloWord'] //列表中的数据
 		}
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handBtnClick = this.handBtnClick.bind(this); //优化
+		this.handleItemDelete = this.handleItemDelete.bind(this);
 	}
 	
 	render() {
@@ -22,52 +26,74 @@ class TodoList extends Component {
 						type="text" 
 						className="input"
 						value={this.state.inputValue} 
-						onChange={this.handleInputChange.bind(this)}	//定义事件
+						onChange={this.handleInputChange}	//定义事件
 					/>
-					<button onClick={this.handBtnClick.bind(this)}>提交</button>
+					<button onClick={this.handBtnClick}>提交</button>
 					<p>{this.state.inputValue}</p>
 
 					<ul>
-						{
-							this.state.list.map((item, index) => {
-								return (
-									<li 
-										key={index} //不建议使用index
-										onClick={this.handleItemDelete.bind(this, index)}  //事件传参
-										dangerouslySetInnerHTML={{__html: item}} //不解析标签  下面的{item}可不写
-									>
-										{/* {item} */}
-									</li>
-								)
-							})
-						}
+						{this.getTodoItem()} {/* 精简写法 拆分为方法 */}
 					</ul>
 
 				</div>
 			</Fragment>
-    )
+    	)
 	}
 
-	handleInputChange(e) {
-		this.setState({
-			inputValue: e.target.value
+	getTodoItem() {
+		return this.state.list.map((item, index) => {
+			return (
+				<ToDoItem 
+					key={index}
+					content={item} //父组件传值 定义属性
+					index={index}
+					deleteItem={this.handleItemDelete}/> //向子组件传递方法
+					
+				// <li 
+				// 	key={index} //不建议使用index
+				// 	onClick={this.handleItemDelete.bind(this, index)}  //事件传参
+				// 	dangerouslySetInnerHTML={{__html: item}} //不解析标签  下面的{item}可不写
+				// >
+				// 	{/* {item} */}
+				// </li>
+			)
 		})
+	}
+
+
+	handleInputChange(e) {
+		//旧版本
+		// this.setState({
+		// 	inputValue: e.target.value
+		// })
+
+		//新版本
+		const value = e.target.value;
+		// this.setState(() => {
+		// 	return {
+		// 		inputValue: value
+		// 	}
+		// })
+		//或者
+		this.setState(() => ({
+			inputValue: value
+		}))
 	}
 
 	handBtnClick() {
-		this.setState({
-			list: [...this.state.list, this.state.inputValue],
+		this.setState((prevState) => ({  //prevState修改之前的数据等同于this.state
+			list: [...prevState.list, prevState.inputValue],
 			inputValue: ''
-		})
+		}))
 	}
 
-	handleItemDelete(index) {
+	handleItemDelete(index) { //接收参数
 		//immutable原则
 		//state 不允许我们做任何的改变
-		const list = [...this.state.list];
-		list.splice(index, 1);
-		this.setState({
-			list: list
+		this.setState((prevState) => {
+			const list = [...prevState.list];
+			list.splice(index, 1);
+			return {list} //list: list es6键值一样 省略
 		})
 		console.log(index)
 	}
